6393f7728e1838dabda6651559dca65f6fd4808b
Seba <sebavergara7109@gmail.com>
2025-03-04 12:43:02 -0300

    chore(types): add TypeScript type definitions

---
+++ src/types/express.d.ts
@@ -0,0 +1,13 @@
+import { Express } from "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: {
+        userId: string;
+      };
+    }
+  }
+}
+
+export {};
